67f6629db09a28597498369ea7392f305eb8e8b6
Mariana <mariana.ospinap@konradlorenz.edu.co>
2024-04-01 18:09:21 -0500

    Se creo la nueva pagina con las dos opciones de juego
    La nueva pagina con la opcion de Guitar Hero y Pokemones fue creada con ambos simbolos funcionando creando ambas ventanas separadas

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,12 @@ function mostrarContinuar() {
     const nombre = document.getElementById('nombreInput').value;
     if (nombre.trim() !== '') {
         // Redirigir a una nueva página en la misma ventana
-        window.location.href = 'otra_pagina.html';
+        window.location.href = 'pagina_dos.html';
     } else {
         alert('Por favor, ingresa tu nombre antes de continuar.');
     }
+}
+
+function abrirNuevaPestana(url) {
+    window.open(url, '_self');
 }
